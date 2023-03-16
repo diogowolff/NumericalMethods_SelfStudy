15638d076f1624cf449701335bedae7cc186426f
diogowolff <54316784+diogowolff@users.noreply.github.com>
2023-03-16 12:46:05 -0300

    finished Q3

--- ProblemSets/PS2.m
+++ ProblemSets/PS2.m
@@ -15,13 +15,17 @@ sigma = .007;
 
 [ShockGrid, MarkovMatrix] = TauchenDiscretizer(7, 3, 0, rho, sigma);
 
-SteadyState = ((zeta-delta)/alpha)^(1/(alpha-1));
+SteadyState = ((zeta+delta)/alpha)^(1/(alpha-1));
 StockGrid = linspace(.75*SteadyState, 1.25*SteadyState, 500);
 [KGrid, KNewGrid, TFPGrid] = meshgrid(StockGrid, StockGrid, exp(ShockGrid));
 
 UtilityMatrix = ((TFPGrid.*KGrid.^alpha + (1-delta).*KGrid - KNewGrid).^(1-mu)-1)./(1-mu);
+UtilityMatrix(TFPGrid.*KGrid.^alpha + (1-delta).*KGrid - KNewGrid <=0 ) = -inf;
 
-Value = zeros(500, 7);
+%% Brute force
+
+%Starting with u(SS)/(1-beta) as it is a good guess
+Value = zeros(500, 7) + ((SteadyState^alpha - delta*SteadyState)^(1-mu)-1)/((1-mu)*(1-beta));
 NewValue = Value;
 error = 100;
 tol = 10e-6;
@@ -31,8 +35,8 @@ tic
 while error > tol
     for i=1:500
         for j=1:7
-            PossibleNewValues = UtilityMatrix(i, i:500, j) + ...
-                beta.*Value(i:500, :)*MarkovMatrix(j,:)';
+            PossibleNewValues = UtilityMatrix(:, i, j) + ...
+                beta.*Value*MarkovMatrix(j,:)';
             NewValue(i,j) = max(PossibleNewValues,[],'all');
         end
     end
@@ -42,4 +46,88 @@ while error > tol
 end
 toc
 
-plot(Value(:,1))
+%7.15 seconds
+
+%% Using monotonicity
+
+%Starting with u(SS)/(1-beta) as it is a good guess
+Value = zeros(500, 7) + ((SteadyState^alpha - delta*SteadyState)^(1-mu)-1)/((1-mu)*(1-beta));
+NewValue = Value;
+error = 100;
+tol = 10e-6;
+iter = 0;
+ChoiceGivenState = zeros(7,1);
+
+
+tic
+while error > tol
+    for i=1:500
+        if i==1
+            for j=1:7
+            PossibleNewValues = UtilityMatrix(:, i, j) + ...
+                beta.*Value*MarkovMatrix(j,:)';
+            [NewValue(i,j), ChoiceGivenState(j)] = max(PossibleNewValues,[],'all');
+            end
+        else   
+            for j=1:7
+            PossibleNewValues = UtilityMatrix(ChoiceGivenState(j):500, i, j) + ...
+                beta.*Value(ChoiceGivenState(j):500, :)*MarkovMatrix(j,:)';
+            [NewValue(i,j), ChoiceGivenState(j)] = max(PossibleNewValues,[],'all');
+            end
+        end
+    end
+    error = max(abs(Value - NewValue),[],'all');
+    Value = NewValue;
+    iter = iter+1;
+end
+toc
+
+% 12.6 seconds; possibly would be faster if I were to parallelize the code.
+
+%Some thoughts: the evaluation over each point of the shock grid is
+%essentially independent, being connected only through the markov matrix.
+%If I reverse the order of iteration to shock(stock) I can just put a
+%parfor on the outside loop and it'll all be parallel.
+
+%% Policy function
+
+Index = zeros(500, 7);
+Choice = zeros(500, 7);
+
+for i=1:500
+    for j=1:7
+        PossibleValues = UtilityMatrix(:, i, j) + ...
+                beta.*Value*MarkovMatrix(j,:)';
+        [~, Index(i, j)] = max(PossibleValues,[],'all');
+        Choice(i,j) = StockGrid(Index(i, j));
+    end
+end
+
+plot(Choice(220:280,:));
+
+% Doing the policy like this was a really bad idea. Creating an actual
+% function would be much better, might do it later.
+
+
+%% Euler errors
+
+ConsumptionToday = (reshape(TFPGrid(1,:,:).*KGrid(1,:,:).^alpha + (1-delta).*KGrid(1,:,:),[500,7]) - Choice);
+
+Index2 = zeros(500, 7);
+Choice2 = zeros(500, 7);
+for i=1:500
+    for j=1:7
+        Index2(i, j) = Index(Index(i,j), j);
+        Choice2(i,j) = StockGrid(Index2(i, j));
+    end
+end
+
+MarginalUtilityTomorrow = (reshape(TFPGrid(1,:,:),[500,7]).*Choice.^alpha + (1-delta).*Choice - Choice2).^(-mu).*...
+    (alpha.*(reshape(TFPGrid(1,:,:),[500,7]).*Choice.^(alpha-1)) + (1-delta));
+ExpecMgUtTomorrow = MarginalUtilityTomorrow*MarkovMatrix';
+EEE = log10(abs(1 - (beta.*ExpecMgUtTomorrow).^(-1/mu)./ConsumptionToday));
+
+plot(EEE);
+
+%It works!
+
